5f89a062bba0e856c5998d90d9b37ff08c0cc09b
LucSedirae <jondeavers@gmail.com>
2025-04-27 17:06:40 -0400

    Added menu music

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -1,7 +1,12 @@
 # UI/main_menu.gd
 extends Control
 
+@onready var boombox = $BackgroundBoombox
 const SCENE_NAME = "main menu"
+
+# System
+func _ready():
+	boombox.play()
 
 # Functions
 func get_scene_name():
